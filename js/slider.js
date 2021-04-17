2ea37d17e3dbd964d90f6c580708dffc2e2142fe
O.G <alexandrgoya@gmail.com>
2021-04-17 11:16:23 +0300

    test-work

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+new Swiper ('.swiper-container', {
+    navigation:{
+        nextE1:'.swiper-button-next',
+        prevE1:'.swiper-button-prev'
+    },
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+        dynamicBullets:true,
+    },
+});
